aa4b3dfaa22c82a81c4d7d805d3ed221c2586e5f
Muhammad Hashim Ali <f223635@cfd.nu.edu.pk>
2025-01-26 10:55:21 +0500

    Task 1 Completed

---
+++ script.js
@@ -0,0 +1,10 @@
+function  playNext() {
+    var audioSource = document.getElementById("audio")
+    audioSource.src = "resources/videoplayback.mp3"
+    audioSource.play()
+    }
+    function  playBack() {
+    var audioSource = document.getElementById("audio")
+    audioSource.src = "resources/videoplayback (1).mp3"
+    audioSource.play()
+    }
