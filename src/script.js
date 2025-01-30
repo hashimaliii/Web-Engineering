ae954baa2877d512ac5d991029829f66b592612f
Muhammad Hashim Ali <hashimtrox@gmail.com>
2025-01-30 13:43:32 +0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -32,7 +32,7 @@ function addNewTask() {
     let newNode = document.createElement('li');
     let taskName = document.getElementById("todolistName").value;
     let recycleBinImage = document.createElement('input');
-    if(taskName.length <= 0){
+    if (taskName.length <= 0) {
         alert("Error: Invalid Task Name")
         return;
     }
@@ -40,7 +40,7 @@ function addNewTask() {
     recycleBinImage.src = ".././resources/recycle-bin.png";
     recycleBinImage.height = 20;
     recycleBinImage.width = 20;
-    recycleBinImage.onclick = function() {deleteElement(newNode)};
+    recycleBinImage.onclick = function () { deleteElement(newNode) };
     newNode.appendChild(document.createTextNode(taskName));
     newNode.appendChild(recycleBinImage)
 
@@ -51,3 +51,48 @@ function deleteElement(newNode) {
     let todolist = document.getElementById("todolist");
     todolist.removeChild(newNode)
 }
+
+// let fruits = ['Apple', 'Banana', 'Orange']
+let fruits = new Array("Apple", "Banana", "Orange");
+console.log(fruits)
+// fruits.splice(1,1,'Apple')
+
+// fruits.slice()
+
+// fruits.forEach((value)=>console.log(value.toUpperCase()))
+
+// console.log(fruits.filter(value=> value == "Apple"))
+
+// let htmlListItems = fruits.map(value => "<li>" + value + "</li>")
+// console.log(htmlListItems)
+
+// result, currentValue, definition, defaultValue
+// let values = [1,2,5,8,9]
+// let result = values.reduce((acc, val,)=>acc * val, 1)
+// console.log(result)
+
+// let result = fruits.reduce((acc, val,)=> "<li>"+ val +"</li>" + acc, "</ul>\n")
+// console.log("<ul>"+result)
+
+// let values = [1,2,5,8,9]
+// console.log(values.reduce((acc, val,)=> val > acc ? acc = val : acc, values[0]))
+
+let values = [[1,2,5,8,9],[9,10]];
+// console.log(values.reduce((acc, val)=> acc.concat(val), []))
+
+// let valueSet = [1,3,5,1];
+// let value = 3;
+// let displayMessage = "Heello" + value;
+// console.log(displayMessage)
+// displayMessage = `Hello ${value}`
+// console.log(displayMessage)
+// console.log(displayMessage[0])
+// console.log(displayMessage.charAt(0))
+// displayMessage[0] = 'a'
+// console.log(displayMessage)
+
+// displayMessage = displayMessage.replace('H', 'a')
+// console.log(displayMessage)
+
+let result = values.join()
+console.log(result)
