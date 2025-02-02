1c15f5b052c1a15abbdb8497e909404fd0d254a4
Muhammad Hashim Ali <hashimtrox@gmail.com>
2025-02-02 10:22:12 +0500

    Task 3: Student Names[checkboxes][edit,view] Offered Courses [List]

--- src/script.js
+++ src/script.js
@@ -1,175 +1,147 @@
-function playNext() {
-    var audioSource = document.getElementById("audio");
-    audioSource.src = "resources/videoplayback.mp3";
-    audioSource.play();
-}
+// Task
+// Page Display
+// Student Names[checkboxes][edit,view] Offered Courses [List]
 
-function playBack() {
-    var audioSource = document.getElementById("audio");
-    audioSource.src = "resources/videoplayback (1).mp3";
-    audioSource.play();
-}
+var studentsList = new Object()
 
-getLocation()
-function getLocation() {
-    const x = document.getElementById("location");
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
-
-function showPosition(position) {
-    const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    const x = document.getElementById("location");
-    x.innerHTML = `Latitude: ${lat}<br>Longitude: ${lng}`;
-}
-
-function addNewTask() {
-    let todolist = document.getElementById("todolist");
-    let newNode = document.createElement('li');
-    let taskName = document.getElementById("todolistName").value;
-    let recycleBinImage = document.createElement('input');
-    if (taskName.length <= 0) {
-        alert("Error: Invalid Task Name")
+function addNewStudent() {
+    let studentName = String(document.getElementById("studentName").value).toUpperCase();
+    if (studentName.length <= 0) {
+        alert("Error: Invalid Student Name")
         return;
     }
+
+    let ol = document.getElementById('studentlist');
+    let node = ol.firstChild.nextSibling;
+    while (node) {
+        if (node.textContent == studentName)
+        {
+            alert(`Error: Student ${studentName} is already registered!`)
+            return;
+        }
+        node = node.nextSibling;
+    }
+
+    let todolist = document.getElementById("studentlist");
+    let newNode = document.createElement('li');
+    let recycleBinImage = document.createElement('input');
+    let radioButton = document.createElement('input');
+    studentsList[studentName] = new Set();
+    radioButton.type = "radio"
+    radioButton.name = "students"
+    radioButton.onclick = function () { checkCheckBoxesForThisStudent(studentName) };
+    radioButton.value = studentName
+    newNode.value = studentName
+    recycleBinImage.value = studentName
     recycleBinImage.type = "image"
     recycleBinImage.src = ".././resources/recycle-bin.png";
     recycleBinImage.height = 20;
     recycleBinImage.width = 20;
-    recycleBinImage.onclick = function () { deleteElement(newNode) };
-    newNode.appendChild(document.createTextNode(taskName));
+    recycleBinImage.onclick = function () { deleteStudentElement(newNode) };
+    newNode.appendChild(radioButton)
+    newNode.appendChild(document.createTextNode(studentName));
     newNode.appendChild(recycleBinImage)
 
     todolist.appendChild(newNode);
 }
 
-function deleteElement(newNode) {
-    let todolist = document.getElementById("todolist");
-    todolist.removeChild(newNode)
+function checkCheckBoxesForThisStudent(studentName) {
+    let courses = studentsList[studentName]
+    let ol = document.getElementById('courselist');
+    let node = ol.firstChild.nextSibling;
+    while (node) {
+        if (node.tagName === 'LI') {
+            if (courses.has(node.textContent))
+            {
+                node.firstChild.checked = true;
+            }
+            else
+            {
+                node.firstChild.checked = false;
+            }
+        }
+        node = node.nextSibling;
+    }
 }
 
-// let fruits = ['Apple', 'Banana', 'Orange']
-// let fruits = new Array("Apple", "Banana", "Orange");
-// console.log(fruits)
-// fruits.splice(1,1,'Apple')
+function deleteStudentElement(newNode) {
+    let todolist = document.getElementById("studentlist");
+    todolist.removeChild(newNode)
+    delete studentsList[newNode.textContent]
+}
 
-// fruits.slice()
+function addNewCourse() {
+    let courseName = String(document.getElementById("courseName").value).toUpperCase();
+    if (courseName.length <= 0) {
+        alert("Error: Invalid Course Name")
+        return;
+    }
 
-// fruits.forEach((value)=>console.log(value.toUpperCase()))
+    let ol = document.getElementById('courselist');
+    let node = ol.firstChild.nextSibling;
+    while (node) {
+        if (node.textContent == courseName)
+        {
+            alert(`Error: Course ${courseName} is already registered!`)
+            return;
+        }
+        node = node.nextSibling;
+    }
 
-// console.log(fruits.filter(value=> value == "Apple"))
+    let todolist = document.getElementById("courselist");
+    let newNode = document.createElement('li');
+    let recycleBinImage = document.createElement('input');
+    let checkBox = document.createElement('input');
+    checkBox.type = "checkbox"
+    checkBox.value = courseName
+    checkBox.id = courseName
+    newNode.id = courseName+"id"
+    checkBox.onclick = function () { addCourseInStudentList(courseName) };
+    recycleBinImage.type = "image"
+    recycleBinImage.src = ".././resources/recycle-bin.png";
+    recycleBinImage.height = 20;
+    recycleBinImage.width = 20;
+    recycleBinImage.onclick = function () { deleteCourseElement(newNode) };
+    newNode.appendChild(checkBox)
+    newNode.appendChild(document.createTextNode(courseName));
+    newNode.appendChild(recycleBinImage)
 
-// let htmlListItems = fruits.map(value => "<li>" + value + "</li>")
-// console.log(htmlListItems)
+    todolist.appendChild(newNode);
+}
 
-// result, currentValue, definition, defaultValue
-// let values = [1,2,5,8,9]
-// let result = values.reduce((acc, val,)=>acc * val, 1)
-// console.log(result)
+function addCourseInStudentList (courseName) {
+    let studentName = null;
+    let ol = document.getElementById('studentlist');
+    let li = [];
+    let node = ol.firstChild.nextSibling;
+    while (node) {
+        if (node.tagName === 'LI') {
+            if (node.firstChild.checked)
+            {
+                studentName = node.textContent
+            }
+            li.push(node);
+        }
+        node = node.nextSibling;
+    }
 
-// let result = fruits.reduce((acc, val,)=> "<li>"+ val +"</li>" + acc, "</ul>\n")
-// console.log("<ul>"+result)
+    let isChecked = document.getElementById(courseName).checked;
+    if (isChecked && studentName != null)
+    {
+        let studentCourseList = studentsList[studentName]
+        studentCourseList.add(courseName)
+    }
+    else if (studentName != null)
+    {
+        studentsList[studentName].delete(courseName)
+    }
+}
 
-// let values = [1,2,5,8,9]
-// console.log(values.reduce((acc, val,)=> val > acc ? acc = val : acc, values[0]))
-
-// let values = [[1,2,5,8,9],[9,10]];
-// console.log(values.reduce((acc, val)=> acc.concat(val), []))
-
-// let valueSet = [1,3,5,1];
-// let value = 3;
-// let displayMessage = "Heello" + value;
-// console.log(displayMessage)
-// displayMessage = `Hello ${value}`
-// console.log(displayMessage)
-// console.log(displayMessage[0])
-// console.log(displayMessage.charAt(0))
-// displayMessage[0] = 'a'
-// console.log(displayMessage)
-
-// displayMessage = displayMessage.replace('H', 'a')
-// console.log(displayMessage)
-
-// let values = [[1,2,5,8,9],[9,10]];
-// let result = values.join()
-// console.log(result)
-
-// let person = {
-//     name: 'Hashim',
-//     age: 25,
-//     isStudent: true,
-//     registeredCourses: {
-//         course1: {
-//             title: 'PF',
-//             isPassed: true
-//         },
-//         course2: {
-//             title: 'OOP',
-//             isPassed: true
-//         }
-//     },
-//     displayName: function () {
-//         return this.name
-//     }
-// };
-
-// let course3 = {
-//     title: 'Web',
-//     isPassed: true
-// }
-// person['registeredCourses']['course3'] = course3
-// person.Prototype.semesterStart = true
-// person.Prototype.greet = function(){
-//     return `Hello ${this.name}`
-// }
-
-// console.log(person.Prototype)
-// console.log(person)
-// console.log(person.displayName())
-// console.log(person.name)
-// console.log(person['name'])
-// delete person['name']
-// console.log(person['registeredCourses']['course1']['title'])
-
-// console.log(Object.keys(person))
-// console.log(Object.values(person))
-// for (key in person){
-//     console.log(person[key]);
-// }
-
-// let {name, isStudent} = person
-// console.log(name, isStudent)
-
-// let currentStudent = {... person}
-// console.log(currentStudent)
-// function add(...val){
-//     return 1;
-// }
-
-// function createPerson(name,isStudent){
-//     this.name = name;
-//     this.isStudent = isStudent;
-// }
-
-// let person3 = new createPerson('Hashim', true)
-// console.log(person3)
-
-// Object.freeze(person)
-// Object.seal(person)
-
-// let person1 = new Object();
-// person1.name = 'Talha'
-// person1.isStudent = true
-// console.log(person1)
-
-// let person2 = Object.create(null);
-// person2.name = 'Yousaf'
-// console.log(person2)
-
-// Task
-// Page Display
-// Student Names[checkboxes][edit,view] Offered Courses [List]
+function deleteCourseElement(newNode) {
+    let courseName = newNode.textContent
+    let todolist = document.getElementById("courselist");
+    todolist.removeChild(newNode)
+    for (const [key, value] of Object.entries(studentsList)) {
+        value.delete(courseName)
+    }
+}
