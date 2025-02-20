ba7d0531b4b3969f5a60b70fbe84a8da3fa353bc
Muhammad Hashim Ali <hashimtrox@gmail.com>
2025-02-20 13:14:05 +0500

    Promise, Local and Session Storage

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-console.log("Before");
+// console.log("Before");
 
 // 1
 // function fetchUser() {
@@ -65,7 +65,52 @@ console.log("Before");
 //     console.log("Nhi chalega" + result)
 // }))
 
-const getAPI = fetch('https://api.github.com/user/hashimaliii')
-getAPI.then((res) => console.log(res))
-    .catch((err) => console.log(err.message))
-console.log("End")
+// Promise.all([promise]).then(function (result) {
+//     console.log("Chal gya" + result)
+// }).catch(function (result) {
+//     console.log("Nhi chalega" + result)
+// });
+
+// const getAPI = fetch('https://api.github.com/users/hashimaliii')
+// getAPI.then((res) => res.json())
+// .then(profile => console.log(profile))
+// .catch((err) => console.log(err.message))
+// console.log("End")
+
+// async function getGitData() {
+//     const getAPI = fetch('https://api.github.com/users/hashimaliii')
+//     const profile = (await getAPI).json()
+//     console.log(profile)
+// }
+
+// getGitData()
+
+// localStorage.setItem("githubURL", "https://api.github.com/users/hashimaliii")
+// sessionStorage.setItem("githubURL", "https://api.github.com/users/hashimaliii")
+
+// sessionStorage.clear()
+// localStorage.clear()
+
+// const users = [
+//     { name: 'Hashim', age: 22 },
+//     { name: 'Sufiyan', age: 21 },
+// ]
+
+// console.log(users)
+// usersJSON = JSON.stringify(users)
+// console.log(usersJSON)
+
+// localStorage.setItem('classStudent', usersJSON)
+// const usersPARSE = JSON.parse(localStorage.getItem('classStudent'))
+// console.log(usersPARSE)
+
+// Task
+// Record Expenses
+// Source: [text box]
+// Amount:  [amount Text box]
+// Spend Income Buttons
+
+// Debit Balance:       Credit Balance
+// [] green and plus     [] red and in minus
+//          Total Amount [Rs. ]
+// History
